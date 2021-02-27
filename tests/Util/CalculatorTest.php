1c3327f32d98516c3f081225ec366f6b04ed42b2
Ryan Weaver <ryan@thatsquality.com>
2021-02-27 15:00:25 -0500

    Step 3: Unit: Add unit test

---
+++ tests/Util/CalculatorTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Tests\Util;
+
+use PHPUnit\Framework\TestCase;
+
+class CalculatorTest extends TestCase
+{
+    public function testSomething(): void
+    {
+        $this->assertTrue(true);
+    }
+}
