b4fa2829d228684358e293ff1d1e4be2abf9c8a8
Ryan Weaver <ryan@thatsquality.com>
2021-02-27 15:00:27 -0500

    Step 7: Foundry: install foundry

--- config/bundles.php
+++ config/bundles.php
@@ -15,4 +15,5 @@ return [
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
     Symfony\UX\Chartjs\ChartjsBundle::class => ['all' => true],
+    Zenstruck\Foundry\ZenstruckFoundryBundle::class => ['all' => true],
 ];
